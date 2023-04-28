b935d69604dbbd23ad0607d4d2cf67d399af0f34
yraffic <yraffic@gmail.com>
2023-04-27 22:26:59 -0300

    style: about and initialized card

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+import { Card } from '../../components/Card';
 import { Header } from '../../components/Header';
 import './styles.scss'
 
@@ -6,6 +7,24 @@ export const Home = ()=>{
   return (
     <div >
      <Header />
+     <div 
+     className='
+      about 
+      flex 
+      align-items 
+      justify-content 
+      flex-direction'
+     >
+        <h2>Nossa empresa</h2>
+        <div className='
+        about-cards 
+        flex
+        align-items 
+        justify-content'
+        >
+          <Card />
+        </div>
+     </div>
     </div>
   );
 }
