700f7f342acd778a01954c3dda9fbcdadbcd196c
yraffic <yraffic@gmail.com>
2023-05-03 20:08:14 -0300

    style: responsiveness formulario done

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,9 +1,9 @@
 import { Button } from '../Button'
 import './styles.scss'
 
-export const Forms = () =>{
+export const Forms = () => {
     return (
-        <form  className='
+        <form className='
                 flex
                 align-items
                 justify-content
@@ -11,11 +11,22 @@ export const Forms = () =>{
                 forms
         '
         >
-            <div className='blur'></div>
-            <input type="text" placeholder='Seu Nome ou Empresa'/>
-            <input type="email" placeholder='Email'/>
-            <input type="number" placeholder='DD + Numero'/>
-            <textarea placeholder='fale sobre sua ideia/problema'/>
+            <input
+                type="text"
+                placeholder='Seu Nome ou Empresa'
+                className='blur'
+            />
+            <input
+                type="email"
+                placeholder='Email'
+            />
+            <input
+                type="text"
+                placeholder='DD + Numero'
+            />
+            <textarea
+                placeholder='fale sobre sua ideia/problema'
+            />
             <Button type='primary'>
                 Enviar
             </Button>
